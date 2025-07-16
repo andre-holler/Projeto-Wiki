6ed9dbd6e3e189675f62c4d5933040f5cb8eb453
IAGO DORIGON ADAMS <iago_adams@estudante.sesisenai.org.br>
2025-07-16 11:13:52 -0300

    update db wiki

--- app/models.py
+++ app/models.py
@@ -1,9 +1,8 @@
+from datetime import datetime
 from app import db
 
 class Wiki(db.Model):
     id = db.Column(db.Integer, primary_key = True)
-    nome = db.Column(db.String)
-    email = db.Column(db.String)
     titulo = db.Column(db.String)
-    mensagem = db.Column(db.String)
-    respostas = db.Column(db.Integer, default=0)
+    texto = db.Column(db.String)
+    data = db.Column(db.DateTime, default=datetime.now)
