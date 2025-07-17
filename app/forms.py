92383f0f17bf855275ed988c83da08eebd88a32f
IAGO DORIGON ADAMS <iago_adams@estudante.sesisenai.org.br>
2025-07-17 09:27:13 -0300

    g

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
-from app import db
-from app.models import Wiki
+from wtforms import StringField, SubmitField, PasswordField
+from wtforms.validators import DataRequired, Email, EqualTo, ValidationError
+from app import db, bcrypt
+from app.models import Wiki, User
 
 class WikiForm(FlaskForm):
     titulo = StringField('Titulo', validators=[DataRequired()])
@@ -18,3 +18,41 @@ class WikiForm(FlaskForm):
     
         db.session.add(wiki)
         db.session.commit
+
+class UserForm(FlaskForm):
+    nome = StringField('Nome', validators=[DataRequired()])
+    email = StringField('E-mail', validators=[DataRequired(),Email()])
+    senha = PasswordField('Senha', validators=[DataRequired()])
+    confirmacao = PasswordField('Senha', validators=[DataRequired(), EqualTo('senha')])
+    btnSubmit=SubmitField('Enviar')
+    
+    def validate_email(self, email):
+        if User.query.filter_by(email=email.data).first():
+            return ValidationError('Usuário já cadastrado com esse E-mail!!!')
+    
+    def save(self):
+        senha = bcrypt.generate_password_hash(self.senha.data.encode('utf-8'))
+        user = User(
+            nome = self.nome.data,
+            email = self.email.data,
+            senha = senha
+        )
+
+        db.session.add(user)
+        db.session.commit()
+        return user
+    
+class LoginForm(FlaskForm):
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    senha = PasswordField('Senha', validators=[DataRequired()])
+    btnSubmit = SubmitField('Login')
+
+    def login(self):
+        user = User.query.filter_by(email=self.email.data).first()
+        if user:
+            if bcrypt.check_password_hash(user.senha, self.senha.data.encode('utf-8')):
+                return user
+            else:
+                raise Exception("Senha incorreta!")
+        else:
+            raise Exception("Usuário não encontrado!")
--- app/views.py
+++ app/views.py
@@ -1,6 +1,20 @@
 from app import app
-from flask import render_template
+from flask import render_template, url_for, request, redirect
+from flask_login import login_user, logout_user, current_user
+from app.forms import UserForm
 
 @app.route('/')
 def homepage():
     return render_template('index.html')
+
+@app.route('/cadastro/', methods=['GET', 'POST'])
+def cadastro():
+    form = UserForm()
+    if form.validate_on_submit():
+        if form.validate_email():
+            user = form.save()
+            login_user(user, remember=True)
+            return redirect(url_for('homepage'))
+        else:
+            return
+    return render_template('cadastro.html', form=form)
