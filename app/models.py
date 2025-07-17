f7b93570cbcc57f6c225583b8c6fde6fcb8ce127
IAGO DORIGON ADAMS <iago_adams@estudante.sesisenai.org.br>
2025-07-17 08:00:24 -0300

    models atualizado para trabalhar no forms

--- app/models.py
+++ app/models.py
@@ -2,7 +2,6 @@ from datetime import datetime
 from app import db
 from flask_login import UserMixin
 
-
 class Wiki(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     titulo = db.Column(db.String)
@@ -13,11 +12,9 @@ class Wiki(db.Model):
     id_user = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
     #Relacionamento
-    comentarios = db.relationship('Comentarios', backref='id_comentario', lazy=True)
+    comentarios = db.relationship('Comentario', backref='wiki', lazy=True)
 
-
-
-class User(db.model, UserMixin):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key = True)
     nome = db.Column(db.String)
     email = db.Column(db.String)
@@ -26,7 +23,7 @@ class User(db.model, UserMixin):
     #Relacionamentos
     wikis = db.relationship('Wiki', backref='id_user', lazy=True)
 
-class Comentario(db.model):
+class Comentario(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     data = db.Column(db.DateTime, default=datetime.now)
     texto = db.Column(db.String)
@@ -34,7 +31,7 @@ class Comentario(db.model):
     #Foreign Keys
     id_user = db.Column(db.Integer, db.ForeignKey('user.id' ), nullable=False)
     id_wiki = db.Column(db.Integer, db.ForeignKey('wiki.id'), nullable=False)
-    id_comentario = db.Column(db.Integer, db.Foreignkey('comentario.id'), nullable=True)
+    id_comentario = db.Column(db.Integer, db.ForeignKey('comentario.id'), nullable=True)
 
     #Relacionamento
-    resposta_comentarios = db.relationship('Comentario', backref='comentario.id', lazy=True)
+    resposta_comentarios = db.relationship('Comentario', backref='comentario_id', lazy=True)
